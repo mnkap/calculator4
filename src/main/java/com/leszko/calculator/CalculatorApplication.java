33e38326b0ee6702db4df2ae572204ca276013c4
mnkap <mnkapezov@gmail.com>
2022-07-07 12:08:34 +0600

    Add calculator4

---
+++ src/main/java/com/leszko/calculator/CalculatorApplication.java
@@ -0,0 +1,28 @@
+package com.leszko.calculator;
+
+import com.hazelcast.client.config.ClientConfig; 
+import org.springframework.boot.SpringApplication; 
+import org.springframework.boot.autoconfigure.SpringBootApplication; 
+import org.springframework.cache.annotation.EnableCaching; 
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootApplication
+@EnableCaching
+public class CalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorApplication.class, args);
+	}
+
+	@Bean 
+	public ClientConfig hazelcastClientConfig() { 
+	   ClientConfig clientConfig = new ClientConfig(); 
+	   clientConfig.getNetworkConfig().addAddress("192.168.1.83"); 
+	   return clientConfig; 
+	}
+
+
+}
+
+
